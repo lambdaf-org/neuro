b4b3a2179ac7e651076bb8bfe533c8851bb0b6e1
191-iota <101976629+191-iota@users.noreply.github.com>
2026-02-06 16:36:29 +0100

    remove comments

--- backend/src/config/middleware.rs
+++ backend/src/config/middleware.rs
@@ -7,18 +7,13 @@ use supabase_jwt::Claims;
 use supabase_jwt::JwksCache;
 use uuid::Uuid;
 
-// TODO: rename to middleware
-// TODO: implement security configurations
-// TODO: implement additional securiy checks for operations involving git
-
-// 1. Implement middleware validation
 pub async fn validate_jwt(token: Option<String>) -> Result<Uuid, Box<dyn std::error::Error>> {
     // 1. Initialize the JWKS cache with your Supabase URL
     let supabase_url = env::var("SUPABASE_URL").expect("Could not find SPABASE_URL env");
     let jwks_url = format!("{supabase_url}/auth/v1/.well-known/jwks.json");
     let jwks_cache = JwksCache::new(jwks_url.as_str());
 
-    // 3. Validate the JWT and extract claims
+    // 2. Validate the JWT and extract claims
     if let Some(v) = token {
         match Claims::from_bearer_token(v.as_str(), &jwks_cache).await {
             Ok(claims) => {
