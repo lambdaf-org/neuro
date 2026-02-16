d9d248bd959bafdfcc633bca4d13522e79d7e3ca
191-iota <101976629+191-iota@users.noreply.github.com>
2026-02-16 05:36:36 +0100

    add asset endpoints and register them, refactor game handler

--- backend/src/repositories/game_repository.rs
+++ backend/src/repositories/game_repository.rs
@@ -32,43 +32,6 @@ pub async fn create_session(
     Ok(handled_result)
 }
 
-// TODO: Create enum for game codes
-pub async fn get_asset_groups(
-    db: &SupabaseClient,
-    game_code: String,
-) -> Result<Vec<AssetGroupRes>, RepoError> {
-    let rows = db
-        .select("asset_groups")
-        .eq("game_code", &game_code)
-        .execute()
-        .await
-        .map_err(|e| {
-            log::error!("Failed fetching asset group: {e}");
-            RepoError::ExtractionError(String::from("Failed asset group"))
-        })?;
-
-    serde_json::from_value(Value::Array(rows))
-        .map_err(|e| RepoError::ExtractionError(e.to_string()))
-}
-
-pub async fn get_game_assets(
-    db: &SupabaseClient,
-    group_id: i32,
-) -> Result<Vec<GameAssetRes>, RepoError> {
-    let rows = db
-        .select("game_assets")
-        .eq("group_id", &group_id.to_string())
-        .execute()
-        .await
-        .map_err(|e| {
-            log::error!("Failed fetching game assets: {e}");
-            RepoError::ExtractionError(String::from("Failed fetching game assets"))
-        })?;
-
-    serde_json::from_value(Value::Array(rows))
-        .map_err(|e| RepoError::ExtractionError(e.to_string()))
-}
-
 pub async fn finalize_session(
     db: &SupabaseClient,
     session_id: Uuid,
--- backend/src/routes/global_routes.rs
+++ backend/src/routes/global_routes.rs
@@ -1,6 +1,7 @@
 use crate::config::middleware;
 use crate::handlers::asset_handler::create_asset_group;
 use crate::handlers::asset_handler::delete_asset_group;
+use crate::handlers::asset_handler::get_asset_groups_by_code;
 use crate::handlers::asset_handler::list_asset_groups;
 use crate::handlers::asset_handler::update_asset_group;
 use crate::handlers::game_handler;
@@ -17,20 +18,20 @@ use actix_web::middleware::Next;
 use actix_web::middleware::from_fn;
 use actix_web::web;
 use game_handler::finalize_session;
-use game_handler::get_game_assets;
 use game_handler::start_game;
 
 use log::error;
 
 pub fn init_admin_scope(cfg: &mut web::ServiceConfig) {
     if std::env::var("ADMIN_API_ENABLED").unwrap_or_default() == "true" {
+        log::info!("Admin API enabled");
         cfg.service(
             web::scope("/admin")
                 .wrap(from_fn(auth_filter))
                 .route("/asset-groups", web::post().to(create_asset_group))
                 .route("/asset-groups", web::get().to(list_asset_groups))
-                .route("/asset-groups", web::put().to(update_asset_group))
-                .route("/asset-groups", web::delete().to(delete_asset_group)),
+                .route("/asset-groups/{id}", web::put().to(update_asset_group))
+                .route("/asset-groups/{id}", web::delete().to(delete_asset_group)),
         );
     }
 }
@@ -41,7 +42,10 @@ pub fn init_api_scope(cfg: &mut web::ServiceConfig) {
         web::scope("/api")
             .wrap(from_fn(auth_filter))
             .route("/game/{code}", web::post().to(start_game))
-            .route("/game/{code}", web::get().to(get_game_assets))
+            .route(
+                "/asset-groups/{code}",
+                web::get().to(get_asset_groups_by_code),
+            )
             .route("/game/session/{id}", web::patch().to(finalize_session))
             .route("/game/session/{id}", web::get().to(get_game_session)),
     );
